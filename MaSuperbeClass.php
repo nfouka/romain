484dfc3d2c1c567a2a537a3c3f615a8b00432712
Nadir Fouka <nfouka@users.noreply.github.com>
2021-12-16 16:27:24 +0100

    MaSuperbeClass
    MaSuperbeClass

---
+++ MaSuperbeClass.php
@@ -0,0 +1,15 @@
+
+<?php 
+
+class MaSuperbeClass {
+
+    function spliterStringRomain($spliter, $utilisateur)
+    {
+        $arrayListeUser = explode($spliter , $utilisateur);
+        return $arrayListeUser[1] ;
+
+    }
+
+}
+
+?>
